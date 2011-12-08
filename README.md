9bcbb640d81b9dc437f208a53386cb9a721cb373
Andrew Freiday <andrewfreiday@gmail.com>
2011-12-08 09:52:47 -0500

    Added support for stereo (2-channel) waveforms

--- README.md
+++ README.md
@@ -9,16 +9,23 @@ This code was adapted by [Andrew Freiday](http://andrewfreiday.com).
 
 ### Requirements
 
-	- PHP 5+
-	- LAME MP3 Encoder (Windows build available from http://www.rarewares.org/mp3-lame-bundle.php)
-	- Web server (Apache, etc -- unless modified to run as a command line script)
+- PHP 5+
+- LAME MP3 Encoder (Windows build available from http://www.rarewares.org/mp3-lame-bundle.php)
+- Web server (Apache, etc -- unless modified to run as a command line script)
 
 ### Usage/Installation
 
-	- Copy to a location on your web server
-	- Ensure that the 'lame' command is accessible and executable from that directory (for Windows systems, place the downloaded .exe and .dll as linked above into that same directory, or modify the script)
-	- Ensure the directory support write persmissions, or specify an alternate temporary output directory that does
-	- Launch the script in your browser (e.g. http://localhost/php-waveform-png.php) and upload your MP3 file along with a selection of output settings (dimensions, colours, etc)
+- Copy to a location on your web server
+- Ensure that the 'lame' command is accessible and executable from that directory (for Windows systems, place the downloaded .exe and .dll as linked above into that same directory, or modify the script)
+- Ensure the directory support write persmissions, or specify an alternate temporary output directory that does - 
+- Launch the script in your browser (e.g. http://localhost/php-waveform-png.php) and upload your MP3 file along with a selection of output settings (dimensions, colours, etc)
+
+### Image generation options
+
+The initial form, when the script is loaded in your browser, allows you to specify some basic parameters for how the image is generated and displayed. Other than the obvious dimensions and colours, there are two advanced options:
+
+- "Draw flat-line" indicates whether or not to draw waveform point details that have a value of 0. Enabling this will allow you to see a flat, zero-level line where there is no audio playing. Otherwise it will be left blank
+- The "stereo waveform" option allows you to generate a single image with both left and right audio channels separately
 
 ### Other notes
 
--- php-waveform-png.php
+++ php-waveform-png.php
@@ -46,7 +46,11 @@
     // copy from temp upload directory to current
     copy($_FILES["mp3"]["tmp_name"], "{$tmpname}_o.mp3");
     
-    $filename = "{$tmpname}.wav";
+		// support for stereo waveform?
+    $stereo = isset($_POST["stereo"]) && $_POST["stereo"] == "on" ? true : false;
+   
+		// array of wavs that need to be processed
+    $wavs_to_process = array();
     
     /**
      * convert mp3 to wav using lame decoder
@@ -55,153 +59,173 @@
      * We don't necessarily need high quality audio to produce a waveform, doing this process reduces the WAV
      * to it's simplest form and makes processing significantly faster
      */
-    exec("lame {$tmpname}_o.mp3 -S -f -m m -b 16 --resample 8 {$tmpname}.mp3 && lame -S --decode {$tmpname}.mp3 {$filename}");
+    if ($stereo) {
+			// scale right channel down (a scale of 0 does not work)
+      exec("lame {$tmpname}_o.mp3 --scale-r 0.1 -m m -S -f -b 16 --resample 8 {$tmpname}.mp3 && lame -S --decode {$tmpname}.mp3 {$tmpname}_l.wav");
+			// same as above, left channel
+      exec("lame {$tmpname}_o.mp3 --scale-l 0.1 -m m -S -f -b 16 --resample 8 {$tmpname}.mp3 && lame -S --decode {$tmpname}.mp3 {$tmpname}_r.wav");
+      $wavs_to_process[] = "{$tmpname}_l.wav";
+      $wavs_to_process[] = "{$tmpname}_r.wav";
+    } else {
+      exec("lame {$tmpname}_o.mp3 -m m -S -f -b 16 --resample 8 {$tmpname}.mp3 && lame -S --decode {$tmpname}.mp3 {$tmpname}.wav");
+      $wavs_to_process[] = "{$tmpname}.wav";
+    }
     
     // delete temporary files
     unlink("{$tmpname}_o.mp3");
     unlink("{$tmpname}.mp3");
     
-    if (!file_exists($filename))
-      exit("Error: WAV file not generated. Please verify directory write permissions and that the LAME encoder is installed.");
-    
-    /**
-     * Below as posted by "zvoneM" on
-     * http://forums.devshed.com/php-development-5/reading-16-bit-wav-file-318740.html
-     * as findValues() defined above
-     * Translated from Croation to English - July 11, 2011
-     */
-    $handle = fopen($filename, "r");
-    // wav file header retrieval
-    $heading[] = fread($handle, 4);
-    $heading[] = bin2hex(fread($handle, 4));
-    $heading[] = fread($handle, 4);
-    $heading[] = fread($handle, 4);
-    $heading[] = bin2hex(fread($handle, 4));
-    $heading[] = bin2hex(fread($handle, 2));
-    $heading[] = bin2hex(fread($handle, 2));
-    $heading[] = bin2hex(fread($handle, 4));
-    $heading[] = bin2hex(fread($handle, 4));
-    $heading[] = bin2hex(fread($handle, 2));
-    $heading[] = bin2hex(fread($handle, 2));
-    $heading[] = fread($handle, 4);
-    $heading[] = bin2hex(fread($handle, 4));
-    
-    // wav bitrate 
-    $peek = hexdec(substr($heading[10], 0, 2));
-    $byte = $peek / 8;
-    
-    // checking whether a mono or stereo wav
-    $channel = hexdec(substr($heading[6], 0, 2));
-    
-    $ratio = ($channel == 2 ? 40 : 80);
-    
-    // start putting together the initial canvas
-    // $data_size = (size_of_file - header_bytes_read) / skipped_bytes + 1
-    $data_size = floor((filesize($filename) - 44) / ($ratio + $byte) + 1);
-    $data_point = 0;
-    
-    /**
-     * DRAW INITAL CANVAS
-     */
-     
     // get user vars from form
     $width = isset($_POST["width"]) ? (int) $_POST["width"] : DEFAULT_WIDTH;
     $height = isset($_POST["height"]) ? (int) $_POST["height"] : DEFAULT_HEIGHT;
     $foreground = isset($_POST["foreground"]) ? $_POST["foreground"] : DEFAULT_FOREGROUND;
     $background = isset($_POST["background"]) ? $_POST["background"] : DEFAULT_BACKGROUND;
     $draw_flat = isset($_POST["flat"]) && $_POST["flat"] == "on" ? true : false;
-    
-    // create original image width based on amount of detail
-    $img = imagecreatetruecolor($data_size / DETAIL, $height);
-    
-    // fill background of image
-    if ($background == "") {
-      imagesavealpha($img, true);
-      $transparentColor = imagecolorallocatealpha($img, 0, 0, 0, 127);
-      imagefill($img, 0, 0, $transparentColor);
-    } else {
-      list($r, $g, $b) = html2rgb($background);
-      imagefilledrectangle($img, 0, 0, (int) ($data_size / DETAIL), $height, imagecolorallocate($img, $r, $g, $b));
-    }
+
+    $img = false;
 
     // generate foreground color
     list($r, $g, $b) = html2rgb($foreground);
     
-    while(!feof($handle) && $data_point < $data_size){
-      if ($data_point++ % DETAIL == 0) {
-        $bytes = array();
+    // process each wav individually
+    for($wav = 1; $wav <= sizeof($wavs_to_process); $wav++) {
+ 
+      $filename = $wavs_to_process[$wav - 1];
+    
+      /**
+       * Below as posted by "zvoneM" on
+       * http://forums.devshed.com/php-development-5/reading-16-bit-wav-file-318740.html
+       * as findValues() defined above
+       * Translated from Croation to English - July 11, 2011
+       */
+      $handle = fopen($filename, "r");
+      // wav file header retrieval
+      $heading[] = fread($handle, 4);
+      $heading[] = bin2hex(fread($handle, 4));
+      $heading[] = fread($handle, 4);
+      $heading[] = fread($handle, 4);
+      $heading[] = bin2hex(fread($handle, 4));
+      $heading[] = bin2hex(fread($handle, 2));
+      $heading[] = bin2hex(fread($handle, 2));
+      $heading[] = bin2hex(fread($handle, 4));
+      $heading[] = bin2hex(fread($handle, 4));
+      $heading[] = bin2hex(fread($handle, 2));
+      $heading[] = bin2hex(fread($handle, 2));
+      $heading[] = fread($handle, 4);
+      $heading[] = bin2hex(fread($handle, 4));
+      
+      // wav bitrate 
+      $peek = hexdec(substr($heading[10], 0, 2));
+      $byte = $peek / 8;
+      
+      // checking whether a mono or stereo wav
+      $channel = hexdec(substr($heading[6], 0, 2));
+      
+      $ratio = ($channel == 2 ? 40 : 80);
+      
+      // start putting together the initial canvas
+      // $data_size = (size_of_file - header_bytes_read) / skipped_bytes + 1
+      $data_size = floor((filesize($filename) - 44) / ($ratio + $byte) + 1);
+      $data_point = 0;
+      
+      // now that we have the data_size for a single channel (they both will be the same)
+      // we can initialize our image canvas
+      if (!$img) {
+        // create original image width based on amount of detail
+				// each waveform to be processed with be $height high, but will be condensed
+				// and resized later (if specified)
+        $img = imagecreatetruecolor($data_size / DETAIL, $height * sizeof($wavs_to_process));
         
-        // get number of bytes depending on bitrate
-        for ($i = 0; $i < $byte; $i++)
-          $bytes[$i] = fgetc($handle);
-        
-        switch($byte){
-          // get value for 8-bit wav
-          case 1:
-            $data = findValues($bytes[0], $bytes[1]);
-            break;
-          // get value for 16-bit wav
-          case 2:
-            if(ord($bytes[1]) & 128)
-              $temp = 0;
-            else
-              $temp = 128;
-            $temp = chr((ord($bytes[1]) & 127) + $temp);
-            $data = floor(findValues($bytes[0], $temp) / 256);
-            break;
+        // fill background of image
+        if ($background == "") {
+          // transparent background specified
+          imagesavealpha($img, true);
+          $transparentColor = imagecolorallocatealpha($img, 0, 0, 0, 127);
+          imagefill($img, 0, 0, $transparentColor);
+        } else {
+          list($br, $bg, $bb) = html2rgb($background);
+          imagefilledrectangle($img, 0, 0, (int) ($data_size / DETAIL), $height * sizeof($wavs_to_process), imagecolorallocate($img, $br, $bg, $bb));
         }
-        
-        // skip bytes for memory optimization
-        fseek($handle, $ratio, SEEK_CUR);
-        
-        // draw this data point
-        // relative value based on height of image being generated
-        // data values can range between 0 and 255
-        $v = (int) ($data / 255 * $height);
-        
-        // don't print flat values on the canvas if not necessary
-        if (!($v / $height == 0.5 && !$draw_flat))
-          // draw the line on the image using the $v value and centering it vertically on the canvas
-          imageline(
-            $img,
-            // x1
-            (int) ($data_point / DETAIL),
-            // y1: height of the image minus $v as a percentage of the height for the wave amplitude
-            $height - $v,
-            // x2
-            (int) ($data_point / DETAIL),
-            // y2: same as y1, but from the bottom of the image
-            $height - ($height - $v),
-            imagecolorallocate($img, $r, $g, $b)
-          );         
-        
-      } else {
-        // skip this one due to lack of detail
-        fseek($handle, $ratio + $byte, SEEK_CUR);
       }
-    }
-    
-    // close and cleanup
-    fclose($handle);
-    unlink("{$tmpname}.wav");
-    
-    header("Content-Type: image/png");
 
+      while(!feof($handle) && $data_point < $data_size){
+        if ($data_point++ % DETAIL == 0) {
+          $bytes = array();
+          
+          // get number of bytes depending on bitrate
+          for ($i = 0; $i < $byte; $i++)
+            $bytes[$i] = fgetc($handle);
+          
+          switch($byte){
+            // get value for 8-bit wav
+            case 1:
+              $data = findValues($bytes[0], $bytes[1]);
+              break;
+            // get value for 16-bit wav
+            case 2:
+              if(ord($bytes[1]) & 128)
+                $temp = 0;
+              else
+                $temp = 128;
+              $temp = chr((ord($bytes[1]) & 127) + $temp);
+              $data = floor(findValues($bytes[0], $temp) / 256);
+              break;
+          }
+          
+          // skip bytes for memory optimization
+          fseek($handle, $ratio, SEEK_CUR);
+          
+          // draw this data point
+          // relative value based on height of image being generated
+          // data values can range between 0 and 255
+          $v = (int) ($data / 255 * $height);
+          
+          // don't print flat values on the canvas if not necessary
+          if (!($v / $height == 0.5 && !$draw_flat))
+            // draw the line on the image using the $v value and centering it vertically on the canvas
+            imageline(
+              $img,
+              // x1
+              (int) ($data_point / DETAIL),
+              // y1: height of the image minus $v as a percentage of the height for the wave amplitude
+              $height * $wav - $v,
+              // x2
+              (int) ($data_point / DETAIL),
+              // y2: same as y1, but from the bottom of the image
+              $height * $wav - ($height - $v),
+              imagecolorallocate($img, $r, $g, $b)
+            );         
+          
+        } else {
+          // skip this one due to lack of detail
+          fseek($handle, $ratio + $byte, SEEK_CUR);
+        }
+      }
+      
+      // close and cleanup
+      fclose($handle);
+
+      // delete the processed wav file
+      unlink($filename);
+      
+    }
+  
     // want it resized?
     if ($width) {
-    
       // resample the image to the proportions defined in the form
       $rimg = imagecreatetruecolor($width, $height);
       // save alpha from original image
       imagesavealpha($rimg, true);
       imagealphablending($rimg, false);
       // copy to resized
-      imagecopyresampled($rimg, $img, 0, 0, 0, 0, $width, $height, $data_size / DETAIL, $height);
-      
+      imagecopyresampled($rimg, $img, 0, 0, 0, 0, $width, $height, imagesx($img), imagesy($img));
     }
+    
+    header("Content-Type: image/png");
     
     imagepng($rimg);
     imagedestroy($rimg);
+    imagedestroy($img);
     
   } else {
     
@@ -225,6 +249,8 @@
     <input type="text" name="background" value="<?php print DEFAULT_BACKGROUND; ?>" /></p>
     
   <p>Draw flat-line? <input type="checkbox" name="flat" /></p>
+  
+  <p>Stereo waveform? <input type="checkbox" name="stereo" /></p>
     
   <p><input type="submit" value="Generate Waveform" /></p>
   
